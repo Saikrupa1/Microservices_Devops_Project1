2cef832180a36f8f11d6a943f7c93e65f49b2f01
Sai Krupa <51315424+Saikrupa1@users.noreply.github.com>
2026-04-08 09:05:53 +0100

    Replace showMessage with loadProducts function

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,14 @@
-function showMessage() {
-    document.getElementById("message").innerText = "Hello from DevOps Project 🎉";
+function loadProducts() {
+    fetch("http://product-service:5000/products")
+        .then(res => res.json())
+        .then(data => {
+            let list = document.getElementById("product-list");
+            list.innerHTML = "";
+
+            data.forEach(p => {
+                let li = document.createElement("li");
+                li.innerText = p.name;
+                list.appendChild(li);
+            });
+        });
 }
